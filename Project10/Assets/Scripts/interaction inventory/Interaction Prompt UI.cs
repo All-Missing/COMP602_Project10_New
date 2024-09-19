4e43e45676f9a98f2f4b142d8cfbc08df3b5e131
fazecheese <faizanahmed.rep@gmail.com>
2024-09-20 11:20:59 +1200

    Update Project 10
    - commit to Faiz
    -New level (3)
    - players basics running

---
+++ Project10/Assets/Scripts/interaction inventory/Interaction Prompt UI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionPromptUI : MonoBehaviour
+{
+   private Camera _mainCam;
+
+   private void Start()
+   {
+      _mainCam = Camera.main;
+   }
+
+   
+}
